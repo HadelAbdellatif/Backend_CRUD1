9ab3fc4426eb6053eddb02a6a2e2d8d669b78eff
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2024-05-14 14:15:07 +0300

    first commit

---
+++ Department_task/Controllers/DepartmentController.cs
@@ -0,0 +1,42 @@
+﻿using Department_task.Data;
+using Department_task.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Department_task.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var department = context.Departments.ToList();
+            return View("Index", department);
+        }
+
+        public IActionResult Details(int id) { 
+            var department_details = context.Departments.Find(id);
+            return View("Details", department_details);
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+
+        public IActionResult Store(Department department) {
+            context.Departments.Add(department);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var department_id = context.Departments.Find(id);
+            context.Departments.Remove(department_id);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Department_task/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Department_task.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Department_task.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=db4799.public.databaseasp.net ; Database=db4799; User Id=db4799; Password=********; Encrypt=False; MultipleActiveResultSets=True;");
+        }
+    }
+}
